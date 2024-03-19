2a741e25e9121eaf168326c93585d081b7e23e5b
Sachin Londhe <lsachin@mollatech.com>
2024-03-19 09:57:17 +0530

    Separated products

---
+++ EngageBot/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require("cors");
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'web')));
+// app.use("/",express.static(path.join(`${__dirname}`, 'web')));
+
+const PORT = 7272;
+
+const HOST = 'localhost' || '127.0.0.1';
+
+app.listen(PORT, () => console.info("SERVER", `Flutter Server listening! [http://${HOST}:${PORT}]`));
+
+module.exports = app;
